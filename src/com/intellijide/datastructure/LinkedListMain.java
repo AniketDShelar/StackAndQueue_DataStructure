537232b4399c20026ae6245dfd27b8897dfa8c56
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-29 11:50:37 +0530

    push operation

--- src/com/intellijide/datastructure/LinkedListMain.java
+++ src/com/intellijide/datastructure/LinkedListMain.java
@@ -5,6 +5,8 @@ public class LinkedListMain {
         LinkedList linkedList1 = new LinkedList();
 
         linkedList1.push(70);
+        linkedList1.push(30);
+        linkedList1.push(56);
         linkedList1.print();
     }
 }
