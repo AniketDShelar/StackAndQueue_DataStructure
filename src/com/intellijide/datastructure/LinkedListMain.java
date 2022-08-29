0761f6ce0dfc7d221dd72d55ff642c3d7a4d3060
Aniket Shelar <theaniketshelar@gmail.com>
2022-08-29 11:10:58 +0530

    Simple linkedlist created

---
+++ src/com/intellijide/datastructure/LinkedListMain.java
@@ -0,0 +1,14 @@
+package com.intellijide.datastructure;
+
+import java.util.LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.add(70);
+
+        System.out.println(linkedList);
+    }
+}
